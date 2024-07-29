2dc4a98be8eafe2ce346e14968e6c8aac55ae162
wild <wildwildwildthings@gmail.com>
2024-07-29 21:00:23 +0700

    work

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,41 @@
+use iced::{application, Element, Task};
+use crate::settings::{SettingsMessage, SettingsView};
+
 mod openai;
 mod settings;
 
-struct Playground {
-
+#[derive(Debug)]
+enum PlaygroundMessage {
+    Settings(SettingsMessage)
 }
 
-fn main() {
-    println!("Hello, world!");
+struct Playground {
+    settings_view: SettingsView
+}
+
+impl Playground {
+    fn new() -> (Self, Task<PlaygroundMessage>) {
+        let (settings_view, task) = SettingsView::new();
+
+        (
+            Self {
+                settings_view,
+            },
+            task.map(PlaygroundMessage::Settings)
+        )
+    }
+
+    fn update(&mut self, message: PlaygroundMessage) {
+
+    }
+
+    fn view(&self) -> Element<PlaygroundMessage> {
+        Element::from(self.settings_view.view())
+            .map(PlaygroundMessage::Settings)
+    }
+}
+
+fn main() -> iced::Result {
+    application("Playground", Playground::update, Playground::view)
+        .run_with(Playground::new)
 }
--- src/settings.rs
+++ src/settings.rs
@@ -1,7 +1,8 @@
 use std::borrow::Cow;
 use std::str::FromStr;
 use iced::futures::TryFutureExt;
-use iced::Task;
+use iced::{Length, Task};
+use iced::widget::{Container, container, text, text_input};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -33,7 +34,9 @@ struct SerializedSettings {
     temperature: Parsable<f32>
 }
 
+#[derive(Debug)]
 pub enum SettingsMessage {
+    Load(SettingsState),
     BaseUrlChanged(String),
     ApiKeyChanged(String),
     ModelChanged(String),
@@ -56,7 +59,7 @@ async fn save_settings(serialized_settings: &SerializedSettings) -> anyhow::Resu
 }
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
-pub struct Settings {
+pub struct SettingsState {
     /// Latest saved settings (to file) if applicable
     saved_settings: SerializedSettings,
     /// Presented in the UI, may not be saved.
@@ -65,32 +68,46 @@ pub struct Settings {
 
 pub enum SettingsView {
     Loading,
-    Loaded(Settings)
+    Loaded(SettingsState)
 }
 
 impl SettingsView {
-    pub fn new() -> (Self, Task<Settings>) {
+    pub fn new() -> (Self, Task<SettingsMessage>) {
         (Self::Loading, Task::future(async move {
             let settings = load_existing_settings().await.unwrap_or_default();
-            Settings {
+            SettingsMessage::Load(SettingsState {
                 saved_settings: settings.clone(),
                 live_settings: settings,
-            }
+            })
         }))
     }
 
-    pub fn settings(&self) -> Cow<Settings> {
+    pub fn settings(&self) -> Cow<SettingsState> {
         match self {
-            SettingsView::Loading => Cow::Owned(Settings::default()),
+            SettingsView::Loading => Cow::Owned(SettingsState::default()),
             SettingsView::Loaded(settings) => Cow::Borrowed(settings)
         }
     }
 
-    pub fn update(&self, message: SettingsMessage) {
-
+    pub fn update(&mut self, message: SettingsMessage) {
+        match message {
+            SettingsMessage::Load(state) => {
+                *self = SettingsView::Loaded(state);
+            }
+            SettingsMessage::BaseUrlChanged(_) => {}
+            SettingsMessage::ApiKeyChanged(_) => {}
+            SettingsMessage::ModelChanged(_) => {}
+            SettingsMessage::MaxTokensChanged(_) => {}
+            SettingsMessage::TemperatureChanged(_) => {}
+        }
     }
 
-    pub fn view(&self) {
-
+    pub fn view(&self) -> Container<SettingsMessage> {
+        container(
+            container(text("Loading Settings...")).center(Length::Fill)
+        )
+            .style(container::rounded_box)
+            .width(Length::Fill)
+            .height(Length::Fill)
     }
 }
